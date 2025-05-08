4fda1293bb08ed980297111086545f312b57d5a3
Shahidullah-Kaisar <shkaisar2002@gmail.com>
2025-05-08 12:44:14 +0600

    All Problem Solve

---
+++ src/index.ts
@@ -0,0 +1,101 @@
+function formatString(input: string, toUpper?: boolean): string {
+  return toUpper != false ? input.toUpperCase() : input.toLowerCase();
+}
+
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
+}
+
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  const result: T[] = [];
+  arrays.forEach((arr) => {
+    arr.forEach((item) => result.push(item));
+  });
+  return result;
+}
+
+
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+  getInfo() {
+    console.log(` "Make: ${this.make}, Year: ${this.year}" `);
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+
+  getModel() {
+    console.log(` "Model: ${this.model}" `);
+  }
+}
+
+
+function processValue(value: string | number): number {
+  return typeof value === "number" ? value * 2 : value.length;
+}
+
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  if (products.length == 0) return null;
+
+  let highest = products[0];
+
+  for (const product of products) {
+    if (product.price > highest.price) {
+      highest = product;
+    }
+  }
+  return highest;
+}
+
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function getDayType(day: Day): string {
+  if (day === Day.Saturday || day === Day.Sunday) {
+    return "Weekend";
+  }
+  return "Weekday";
+}
+
+
+async function squareAsync(n: number): Promise<number> {
+  return new Promise<number>((resolve, reject) => {
+    setTimeout(() => {
+      if (n < 0) {
+        reject(new Error("Negative number not allowed"));
+      } else {
+        resolve(n * n);
+      }
+    }, 1000);
+  });
+}
